98ea93acaddd192786f964220b3dec003c438108
Admin <admin@Admins-MacBook-Air.local>
2024-08-22 21:28:45 -0400

    Task 2 Updated

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Write your JavaScript code here!
 
+const { formSubmission } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
     form.addEventListener("submit", function(event) {
         event.preventDefault();
         let pilotName = document.querySelector("input[name=pilotName]").value;
-        
-    })
+        let copilotName = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+        let list = document.getElementById("faultyItems");
+        formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoLevel);
+    });
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
